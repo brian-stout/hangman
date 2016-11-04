e4b976263cf5a2832fe914fc1cb6707a873ce090
Brian Stout <brianjonstout@gmail.com>
2016-11-03 23:38:47 -0400

    Implement getline.  Implement a word count to randomly select a line later.  Open and closed the file twice because fstream is scaring me.

--- hangman.c
+++ hangman.c
@@ -1,3 +1,4 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -21,4 +22,30 @@ int main(void)
 		perror("Could not open .words file");
 		return EX_NOINPUT;
 	}
+
+	size_t line_count = 0;
+	int ch;
+
+	while ((ch = fgetc(dictionary)) != EOF){
+		if(ch == '\n'){
+			++line_count;
+		}
+	}
+
+	fclose(dictionary);
+	dictionary = fopen(home_directory, "r");
+
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+
+	printf("%zu\n", line_count);
+
+	while ((read = getline(&line, &len, dictionary)) != -1){
+		printf("%s", line);
+	}
+	
+	//getline() automatically mallocs the *line for variable char length 
+	free(line);
+	fclose(dictionary);
 }
