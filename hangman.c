7a8bfc09d48bda6f222c3a07be529e5202b6929a
Brian Stout <brianjonstout@gmail.com>
2016-11-04 00:06:39 -0400

    Remove the read variable from the getline() because it was unnessecary for the program.  All it does is count how many characters are in the line, which I assume is so the function can malloc properly.

--- hangman.c
+++ hangman.c
@@ -15,7 +15,10 @@ int main(void)
 
 	char home_directory[16];
 
+	//Copies whats in envptr to home_directory buffer so we don't write over env variables
 	strncpy(home_directory, envptr, strlen(envptr));
+
+	//Makes full directory path to .hangman file
 	strncat(home_directory, "/.hangman", sizeof(home_directory));
 
 	FILE *dictionary = fopen(home_directory, "r");
@@ -24,12 +27,11 @@ int main(void)
 		return EX_NOINPUT;
 	}
 
-
 	int line_count = 0;
 	int ch;
 
 	//Gets the line count to be used to pick a random line later
-	//Could be implemented into a function
+	//TODO: Could be implemented into a function
 	while ((ch = fgetc(dictionary)) != EOF){
 		if(ch == '\n'){
 			++line_count;
@@ -40,16 +42,13 @@ int main(void)
 	fclose(dictionary);
 	dictionary = fopen(home_directory, "r");
 
-
-
-	printf("DEBUG: %d\n", line_count);
-
 	//Sets the seed for the random number generator
 	srand(time(NULL));
 	int rand_line_number;
 
 	//Determines which line will be pulled out from the file.
 	//The +1 includes the final number
+	//TODO: Can be put in function.. maybe unnesscary?
 	rand_line_number = rand() % line_count + 1;
 
 	//Sets line_count to zero again so it can be reused
@@ -58,16 +57,16 @@ int main(void)
 	//Prepping getline function
 	char *word = NULL;
 	size_t len = 0;
-	ssize_t read;
 
 	//Reads through each line of a file.
 	//getline() automatically malocs the *line for a variable char length
 	//If the line matches rand_line_number than the loop is broke so we can have our word
-	while ((read = getline(&word, &len, dictionary)) != -1){
+	//TODO: Can probably be put in a function
+	while ((getline(&word, &len, dictionary)) != -1){
 		++line_count;
 		if(line_count == rand_line_number){
 			//Removing the newline from the word
-			//word[strlen(word) - 1] = '\0';
+			word[strlen(word) - 1] = '\0';
 			break;
 		}
 	}
